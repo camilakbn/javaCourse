9376112449a38e3ebba244de58ed827f1b62f92f
camilakbn <canmilabrito@gmail.com>
2026-09-25 18:54:04 +0100

    add files

--- src/classe/DesafioModulo/Pessoa.java
+++ src/classe/DesafioModulo/Pessoa.java
@@ -5,14 +5,18 @@ public class Pessoa {
     String nome;
     double peso;
 
-    //add o metodo comer, que vai receber como parâmetro um outro objeto da classe comida
-    //a classe comida também vai ter um nome e peso
-    //quando passar o objeto comida para o metodo comer, de dentro da pessoa
-    //vai pegar o peso da comida e acrescentar ao peso da pessoa
-    //de modo que a pessoa vai ganhando peso
-    //a classe jantar (metodo main), você vai instanciar uma comida
-    //ex: nome feijao, peso 0,223kg, cria uma segunda comida
-    //criar uma pessoa (pessoa vai esperar receber um objeto do tipo comida
-    //e dentro desse metodo você vai pegar o peso da comida e acrescentar no peso da pessoa
-    //no final mostrar o peso antes e depois
+    Pessoa(String nome, double peso){
+        this.nome = nome;
+        this.peso = peso;
+    }
+
+    void comer(Comida comida){
+        if(comida != null){
+            this.peso += comida.peso;
+        }
+    }
+
+    String apresentar(){
+        return "Olá eu sou o " + nome + " e tenho " + peso + " Kgs.";
+    }
 }
